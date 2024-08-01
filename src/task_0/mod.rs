dbab1ebecd9fe3fd224063ad1b1db21593c33255
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2024-08-01 10:18:31 +0200

    Løser oppgave 0

--- src/task_0/mod.rs
+++ src/task_0/mod.rs
@@ -7,25 +7,40 @@ pub fn main() {
 }
 
 /// Definer en `enum` som heter `Color`:
-// todo!(Implementer Color)
+enum Color {
+    White,
+    Black,
+}
 
 /// Definer en `struct` som heter `Pawn`:
-// todo!(Implementer Pawn)
+struct Pawn {
+    color: Color,
+    position: (u8, u8),
+}
+
+impl Pawn {
+    fn new(color: Color, position: (u8, u8)) -> Self {
+        Pawn { color, position }
+    }
+    fn print(&self) -> char {
+        match self.color {
+            Color::White => '♟',
+            Color::Black => '♙',
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
-    //use super::{Pawn, Color};
+    use super::{Color, Pawn};
 
     #[test]
     fn pawn_struct_and_color_enum_exists() {
-        /*
         let white_pawn = Pawn::new(Color::White, (0, 1));
         assert_eq!(white_pawn.print(), '♟');
         assert_eq!(white_pawn.position, (0, 1));
         let black_pawn = Pawn::new(Color::Black, (0, 6));
         assert_eq!(black_pawn.print(), '♙');
         assert_eq!(black_pawn.position, (0, 6));
-         */
-        todo!() // Denne gjør at testen kjører rødt. Fjern denne
     }
 }
